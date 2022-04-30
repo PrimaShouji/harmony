babcaa4876005079ae9145781f522a949bc3acec
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-04-30 10:18:23 -0700

    Add message component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,55 @@
 <script lang="ts">
-	export let name: string;
+  import MessageGroup from "./MessageGroup.svelte";
+
+  export let name: string;
+
+  const messages = [
+    {
+      avatar:
+        "https://cdn.discordapp.com/avatars/128581209109430272/acb5a845a8221b50e523a763c000765a.webp?size=40",
+      name: "Name",
+      time: new Date(),
+      message: "message",
+    },
+  ];
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello, {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
 </main>
+<div>
+  {#each messages as m}
+    <MessageGroup
+      avatar={m.avatar}
+      name={m.name}
+      time={m.time}
+      messages={[m.message]}
+    />
+  {/each}
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
---
+++ src/MessageGroup.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  export let avatar: string;
+  export let name: string;
+  export let time: Date;
+  export let messages: string[];
+</script>
+
+<div class="message-group">
+  <img src={avatar} alt="profile" />
+  <div>
+    <p class="name">{name}<span class="time">{time.toLocaleString()}</span></p>
+    {#each messages as m}
+      <p class="content">{m}</p>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .message-group {
+    display: flex;
+    text-align: left;
+  }
+
+  img {
+    width: 40px;
+    height: 40px;
+    margin-top: 4px;
+    margin-right: 8px;
+  }
+
+  .name {
+    margin: 0;
+    vertical-align: top;
+    font-weight: 600;
+  }
+
+  .time {
+    margin-left: 8px;
+    font-size: 10pt;
+    font-weight: 400;
+  }
+
+  .content {
+    margin: 0;
+    padding-top: 4px;
+  }
+</style>
